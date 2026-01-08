53d9756f64ed3538c89b384b60e81b9f74924284
Mvnx-debug <marcosnalin3@gmail.com>
2026-01-08 18:34:21 -0300

    feat(routes): add protected routes with role-based access
    - Implement auth routes (sign-up, sign-in) via Better Auth
    - Protect /users/me route with requireAuth
    - Protect /users route with requireRole(['OWNER'])
    - Register auth and users routes in main app
    - Add Drizzle Kit scripts to package.json

---
+++ src/modules/users/users.routes.ts
@@ -0,0 +1,17 @@
+import { Elysia } from 'elysia'
+import { requireAuth, requireRole } from '@/shared/auth'
+
+export const usersRoutes = new Elysia({ prefix: '/users'})
+    .use(requireAuth)
+    .get('/me', ({ user }) => ({
+        id: user.id,
+        email: user.email,
+        role: user.role,
+    }))
+
+    //rota Somente Owners
+    .use(requireRole(['OWNER']))
+    .get('/', () => ({
+        message: 'Lista de todos os usuários (somente para owners)',
+        users: [], //conectar com o service depois
+    }))
